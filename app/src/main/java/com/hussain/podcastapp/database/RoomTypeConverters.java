adfd3738e743d7524710713cdda696ea844beb88
akramhussain7 <akram@goflytta.com>
2018-10-05 18:26:54 +0530

    fix : Fix bug in PlayerActivity for playback controls

--- app/src/main/java/com/hussain/podcastapp/database/RoomTypeConverters.java
+++ app/src/main/java/com/hussain/podcastapp/database/RoomTypeConverters.java
@@ -3,7 +3,6 @@ package com.hussain.podcastapp.database;
 import android.arch.persistence.room.TypeConverter;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.hussain.podcastapp.model.PodcastImage;
 
@@ -18,14 +17,9 @@ public class RoomTypeConverters {
         if (data == null) {
             return Collections.emptyList();
         }
-
         Type listType = new TypeToken<List<PodcastImage>>() {
         }.getType();
-        GsonBuilder gson = new GsonBuilder();
-        gson.registerTypeAdapter(PodcastImage.class, new InterfaceAdapter());
-        Gson gson1 = gson.create();
-        return gson1.fromJson(data, listType);
-
+        return new Gson().fromJson(data, listType);
     }
 
     @TypeConverter
